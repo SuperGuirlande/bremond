a04fe88bbf35f9a007e51212be5b1a98f6ff7463
SuperGuirlande <dsj.gamemaster@gmail.com>
2024-11-21 11:14:29 +0100

    Page service + video hero home

--- project/main/views.py
+++ project/main/views.py
@@ -7,3 +7,11 @@ def index(request):
         'user': user,
     }
     return render(request, 'main/index.html', context)
+
+
+def services(request):
+    user = request.user
+    context = {
+        'user': user,
+    }
+    return render(request, 'main/services.html', context)
